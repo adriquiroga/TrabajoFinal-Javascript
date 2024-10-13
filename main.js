5faa37e179240af307160855de82007d16e35c8e
adriquiroga <adrianaquirogag@gmail.com>
2024-10-13 15:07:17 -0300

    saque imagen de swal porque no se muestra

--- main.js
+++ main.js
@@ -9,7 +9,6 @@ document.addEventListener('DOMContentLoaded', function() {
   if (!swalShown) {
     Swal.fire({
       title: '<span class="tit-bienvenida">Te damos la bienvenida a nuestro sitio de cosmética natural</span>',
-      imageUrl: '/TrabajoFinal-Javascript/assets/logo.png',
       imageWidth: 150, 
       imageHeight: 150,
       confirmButtonText: 'Aceptar'
